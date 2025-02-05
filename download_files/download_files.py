e7938bd4d9c7f62e01f7c0fcd8ae319f1d2ef50e
unknown <rammer94@gmail.com>
2025-02-05 00:05:03 -0400

    test photos 15

--- download_files/download_files.py
+++ download_files/download_files.py
@@ -7,7 +7,7 @@ from googleapiclient.errors import HttpError
 from authentication import authenticate
 
 
-def download_media_item(request):
+def download_media_item(request, callback):
     """
     Descarga un archivo (foto o video) desde Google Photos.
     
@@ -18,36 +18,37 @@ def download_media_item(request):
 
     download_dir="downloads"
 
-    media_item = authenticate(request)
+    media_item = authenticate(request, callback)
+    return media_item
 
-    try:
-        # Crear el directorio de descargas si no existe
-        os.makedirs(download_dir, exist_ok=True)
+    # try:
+    #     # Crear el directorio de descargas si no existe
+    #     os.makedirs(download_dir, exist_ok=True)
         
-        # Obtener la URL base y el nombre del archivo
-        base_url = media_item.get("baseUrl")
-        filename = media_item.get("filename")
+    #     # Obtener la URL base y el nombre del archivo
+    #     base_url = media_item.get("baseUrl")
+    #     filename = media_item.get("filename")
         
-        if not base_url or not filename:
-            raise ValueError("El mediaItem no contiene baseUrl o filename.")
+    #     if not base_url or not filename:
+    #         raise ValueError("El mediaItem no contiene baseUrl o filename.")
         
-        # Construir la URL de descarga (agregar parámetro para máxima resolución)
-        download_url = f"{base_url}=d"  # "=d" descarga el archivo en su resolución original
+    #     # Construir la URL de descarga (agregar parámetro para máxima resolución)
+    #     download_url = f"{base_url}=d"  # "=d" descarga el archivo en su resolución original
         
-        # Ruta completa del archivo descargado
-        file_path = os.path.join(download_dir, filename)
+    #     # Ruta completa del archivo descargado
+    #     file_path = os.path.join(download_dir, filename)
         
-        # Descargar el archivo
-        response = requests.get(download_url)
-        response.raise_for_status()  # Lanza una excepción si la descarga falla
+    #     # Descargar el archivo
+    #     response = requests.get(download_url)
+    #     response.raise_for_status()  # Lanza una excepción si la descarga falla
         
-        # Guardar el archivo en el directorio de descargas
-        with open(file_path, "wb") as file:
-            file.write(response.content)
+    #     # Guardar el archivo en el directorio de descargas
+    #     with open(file_path, "wb") as file:
+    #         file.write(response.content)
         
-        return file_path
+    #     return file_path
     
-    except Exception as e:
-        print(f"Error al descargar el archivo {filename}: {str(e)}")
-        return None
+    # except Exception as e:
+    #     print(f"Error al descargar el archivo {filename}: {str(e)}")
+    #     return None
 
--- main.py
+++ main.py
@@ -2,29 +2,70 @@ from fastapi import FastAPI, Request, Response, Depends, status
 from fastapi.responses import RedirectResponse, JSONResponse
 
 from authentication import request_creds, authenticate
-app = FastAPI()
+from download_files import download_media_item
 
 
-@app.get("/auth/video")
+app = FastAPI(
+    title="SharpPixAI - Google Auth API",
+    description="""
+    Esta API permite la autenticación con Google photos para gestionar los archivos de los clientes en SharpPixAI.
+    
+    **Funciones principales:**
+    - Autenticación para la compresion de videos o imágenes o eliminar duplicados.
+    - Manejo de callbacks para recibir tokens.
+
+    
+    """,
+    version="1.0.0",
+    contact={
+        "name": "ShapPixAI",
+        "url": "https://ShapPixAI.com",
+        "email": "rammer@rammerbot.com"
+    },
+    license_info={
+        "name": "ShapPixAI - Google photos Auth",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+    docs_url="/docs",  # Documentacion con Swagger UI
+    redoc_url="/redoc"  # Documentacion con ReDoc
+)
+
+tags_metadata = [
+    {
+        "name": "Autenticación segun la funcionalidad",
+        "description": "Endpoints para autenticar con Google.",
+        "externalDocs": {
+            "description": "Más información",
+            "url": "https://developers.google.com/identity"
+        }
+    },
+    {
+        "name": "Callbacks",
+        "description": "Manejo de respuestas desde Google y procesamiento de funcionalidades."
+    }
+]
+
+
+@app.get("/auth/video", tags=['Autentication'])
 async def auth_google_video(request: Request):
     return request_creds(request, 'video')
 
-@app.get("/auth/image")
+@app.get("/auth/image", tags=['Autentication'])
 async def auth_google_image(request: Request):
     return request_creds(request, 'image')
 
-@app.get("/auth/duplicate")
+@app.get("/auth/duplicate", tags=['Autentication'])
 async def auth_google_duplicate(request: Request):
     return request_creds(request, 'duplicate')
 
-@app.get("/callback_video")
+@app.get("/callback_video", tags=['callback'])
 async def callback_video(request: Request):
-    return authenticate(request, 'video')
+    return download_media_item(request, 'video')
 
-@app.get("/callback_image")
+@app.get("/callback_image", tags=['callback'])
 async def callback_image(request: Request):
     return authenticate(request, 'image')
 
-@app.get("/callback_duplicate")
+@app.get("/callback_duplicate", tags=['callback'])
 async def callback_duplicate(request: Request):
     return authenticate(request, 'duplicate')
